d797baa8450e2bfab57ac53555962c155860261e
Erwin Bohnert <ejbohnert@yahoo.com>
2018-06-19 19:44:01 -0500

    finished part 2

--- src/CheeseMVC/Migrations/20180616210138_ModifyCheese.Designer.cs
+++ src/CheeseMVC/Migrations/20180616210138_ModifyCheese.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CheeseMVC.Migrations
 {
     [DbContext(typeof(CheeseDbContext))]
-    partial class CheeseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180616210138_ModifyCheese")]
+    partial class ModifyCheese
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
